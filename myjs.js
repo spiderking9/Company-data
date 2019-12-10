6422a79d1dfa689f0068ddc13d44939cfa25d6ac
Lukasz <spiderking@interia.pl>
2019-12-10 20:13:01 +0100

    sort finish, paginate not finish

--- myjs.js
+++ myjs.js
@@ -1,83 +1,76 @@
 var d = parseFloat(new Date().getMonth() + 1);
-fetch('https://recruitment.hal.skygate.io/companies').then(data => data.json()).then(response => {
-    response.forEach(company => {
-        var id = company.id;
-        var name = company.name;
-        var city = company.city;
-        var output = `<tr>
-            <th>id</th>
-            <th>name</th>
-            <th>city</th>
-            <th>total income</th>
-            <th>average income</th>
-            <th>last month income</th>
-        </tr>`;
-        fetch('https://recruitment.hal.skygate.io/incomes/' + id).then(data => data.json()).then(response => {
-            var income = Math.round(response.incomes.reduce((a, b) => a + parseFloat(b.value), 0) * 100) / 100;
-            var average = Math.round((income / 49) * 100) / 100;
-            var lastMonth = Math.round(response.incomes.map((id) => parseFloat(id.date.substring(5, 7)) === d ? id.value : 0).reduce((a, b) => {
-                return a + parseFloat(b);
-            }, 0) * 100) / 100;
-            this.output = `
-                            <tr class='package'>
-                                <td class='id'>${id}</td>
-                                <td class='name'>${name}</td>
-                                <td class='city'>${city}</td>
-                                <td class='income'>${income}</td>
-                                <td class='average'>${average}</td>
-                                <td class='lastMonth'>${lastMonth}</td>
-                            </tr>`;
-            document.getElementById('data').innerHTML += this.output;
+var companyDate = [];
+var dane = [];
+var output;
 
-            document.getElementById('data').rows.length;
-            $(function () {
-                var options = {
-                    valueNames: ['id', 'name', 'city', 'income', 'average', 'lastMonth']
-                };
-                var userList = new List('search-results', options);
+//fetch from json
+fetch('https://recruitment.hal.skygate.io/companies')
+    .then(data => data.json()).then(resp => {
+        resp.forEach(company => {
+            var id = company.id;
+            var name = company.name;
+            var city = company.city;
+            dane.push(id);
+            fetch('https://recruitment.hal.skygate.io/incomes/' + id).then(data => data.json()).then(response => {
+                var income = Math.round(response.incomes.reduce((a, b) => a + parseFloat(b.value), 0) * 100) / 100;
+                var average = Math.round((income / 49) * 100) / 100;
+                var lastMonth = Math.round(response.incomes.map((id) => parseFloat(id.date.substring(5, 7)) === d ? id.value : 0).reduce((a, b) => {
+                    return a + parseFloat(b);
+                }, 0) * 100) / 100;
 
-                //sort
-                userList.on("updated", function () {
-                    $('.sort').each(function () {
-                        if ($(this).hasClass("asc")) {
-                            console.log('asc');
-                            $(this).find(".fa").addClass("fa-sort-asc").removeClass("fa-sort-desc").show();
-                        } else if ($(this).hasClass("desc")) {
-                            console.log('desc');
-                            $(this).find(".fa").addClass("fa-sort-desc").removeClass("fa-sort-asc").show();
-                        } else {
-                            console.log('else');
-                            $(this).find(".fa").hide();
-                        }
-                    })
-                })
+                companyDate.push({ id, name, city, income, average, lastMonth });
             });
-
         });
-    })
+    });
 
-});
+var x=0;
+setTimeout(function () {
+        if(x===0) {startSend(); x=1;}
+}, 500);
 
+function startSend() {
+    $(function() {
+            $('#data th').on('click', function() {
+                var attr = $(this).attr('data-attr');
+                var asc = (!$(this).attr('data-asc'));
+                
+                $('#data th').each(function() {
+                    $(this).removeAttr('data-asc');
+                });
 
-
-
-var ids = 210
-async function getIncomes(id) {
-    const response = await fetch('https://recruitment.hal.skygate.io/incomes/' + id);
-    const json = await response.json();
-    var income = Math.round(json.incomes.reduce((a, b) => a + parseFloat(b.value), 0) * 100) / 100;
-    var average = Math.round((income / 49) * 100) / 100;
-    var lastMonth = Math.round(json.incomes.map((id) => parseFloat(id.date.substring(5, 7)) === d ? id.value : 0)
-        .reduce((a, b) => {
-            return a + parseFloat(b);
-        }, 0) * 100) / 100;
-    console.log(income);
-    return {
-        income,
-        average,
-        lastMonth
-    };
-
-
+                if (asc) {
+                    $(this).attr('data-asc', 'asc');
+                }
+        
+                sortRes(attr, asc);
+            });
+                
+            sendRes();
+        });
 }
-console.log(getIncomes(ids));
+
+function sortRes(prop, asc) {
+    companyDate.sort(function(first, secend) {
+        if (asc) {
+            return (first[prop] > secend[prop]) ? 1 : ((first[prop] < secend[prop]) ? -1 : 0);
+        } else {
+            return (secend[prop] > first[prop]) ? 1 : ((secend[prop] < first[prop]) ? -1 : 0);
+        }
+    });
+    sendRes();
+}
+
+function sendRes () {
+    var html = '';
+    for (var i=0;i<10;i++) {
+        html += '<tr>'
+            +'<td>'+companyDate[i].id+'</td>'
+            +'<td>'+companyDate[i].name+'</td>'
+            +'<td>'+companyDate[i].city+'</td>'
+            +'<td>'+companyDate[i].income+'</td>'
+            +'<td>'+companyDate[i].average+'</td>'
+            +'<td>'+companyDate[i].lastMonth+'</td>'
+        +'</tr>';
+    }
+    $('#results').html(html);
+}
